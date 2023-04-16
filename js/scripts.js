c20c23a201efd971054d827986e8eec3fc248950
ajulkjose246 <mail.ajulkjose@gmail.com>
2023-04-16 12:49:52 +0530

    dark mode added

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,37 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+$(window).on('load', function () {
+        $('.loader_bg').fadeToggle();
+});
+$(document).ready(() => {
+    // const themeMode = document.cookie.split("; ").find(row => row.startsWith("themeMode="))?.split("=")[1];
+    const themeMode = localStorage.getItem("themeMode");
+    // console.log(themeMode)
+    $(".theme").remove()
+    if (themeMode == "Dark") {
+        document.body.classList.toggle("dark-theme");
+        $(".theme").remove()
+        $(".modeChange").append("<li class='theme nav-item'><a class='nav-link'><i class='bi bi-brightness-high-fill icon'></i><span class='text nav-text'>Light Mode</span></a></li>");
+        // document.cookie = "themeMode=Dark";
+        localStorage.setItem("themeMode", "Dark");
+
+    } else {
+        $(".theme").remove()
+        $(".modeChange").append("<li class='theme nav-item'><a class='nav-link'><i class='bi bi-moon-fill icon'></i><span class='text nav-text'>Dark Mode</span></a></li>")
+        // document.cookie = "themeMode=Light";
+        localStorage.setItem("themeMode", "Light");
+    }
+    $(".modeChange").click(() => {
+        document.body.classList.toggle("dark-theme");
+        if ($('body').hasClass('dark-theme')) {
+            $(".theme").remove()
+            $(".modeChange").append("<li class='theme nav-item'><a class='nav-link'><i class='bi bi-brightness-high-fill icon'></i><span class='text nav-text'>Light Mode</span></a></li>");
+            // document.cookie = "themeMode=Dark";
+            localStorage.setItem("themeMode", "Dark");
+        } else {
+            $(".theme").remove()
+            $(".modeChange").append("<li class='theme nav-item'><a class='nav-link'><i class='bi bi-moon-fill icon'></i><span class='text nav-text'>Dark Mode</span></a></li>")
+            // document.cookie = "themeMode=Light";
+            localStorage.setItem("themeMode", "Light");
+        }
+
+    })
+})
