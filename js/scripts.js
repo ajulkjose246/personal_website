c108e76a75f38c70d1c9c6d6f4db08115b5f73e3
ajulkjose246 <mail.ajulkjose@gmail.com>
2023-12-18 18:36:34 +0530

    mode change updated and added resume view option

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,15 @@
 $(window).on('load', function () {
-        $('.loader_bg').fadeToggle();
+    $('.loader_bg').fadeToggle();
 });
 $(document).ready(() => {
-    // const themeMode = document.cookie.split("; ").find(row => row.startsWith("themeMode="))?.split("=")[1];
-    const themeMode = localStorage.getItem("themeMode");
-    // console.log(themeMode)
-    $(".theme").remove()
-    if (themeMode == "Dark") {
-        document.body.classList.toggle("dark-theme");
-        $(".theme").remove()
-        $(".modeChange").append("<li class='theme nav-item'><a class='nav-link'><i class='bi bi-brightness-high-fill icon'></i><span class='text nav-text'>Light Mode</span></a></li>");
-        // document.cookie = "themeMode=Dark";
-        localStorage.setItem("themeMode", "Dark");
-
-    } else {
-        $(".theme").remove()
-        $(".modeChange").append("<li class='theme nav-item'><a class='nav-link'><i class='bi bi-moon-fill icon'></i><span class='text nav-text'>Dark Mode</span></a></li>")
-        // document.cookie = "themeMode=Light";
-        localStorage.setItem("themeMode", "Light");
-    }
-    $(".modeChange").click(() => {
-        document.body.classList.toggle("dark-theme");
-        if ($('body').hasClass('dark-theme')) {
-            $(".theme").remove()
-            $(".modeChange").append("<li class='theme nav-item'><a class='nav-link'><i class='bi bi-brightness-high-fill icon'></i><span class='text nav-text'>Light Mode</span></a></li>");
-            // document.cookie = "themeMode=Dark";
-            localStorage.setItem("themeMode", "Dark");
-        } else {
-            $(".theme").remove()
-            $(".modeChange").append("<li class='theme nav-item'><a class='nav-link'><i class='bi bi-moon-fill icon'></i><span class='text nav-text'>Dark Mode</span></a></li>")
-            // document.cookie = "themeMode=Light";
-            localStorage.setItem("themeMode", "Light");
-        }
-
-    })
-})
+const themeMode = localStorage.getItem("themeMode");
+if (themeMode === "Dark") {
+    document.body.classList.add("dark-theme");
+}
+$("#darkModeToggle").prop("checked", themeMode === "Dark");
+$("#darkModeToggle").change(() => {
+    document.body.classList.toggle("dark-theme");
+    const isDarkMode = document.body.classList.contains("dark-theme");
+    localStorage.setItem("themeMode", isDarkMode ? "Dark" : "Light");
+});
+});
